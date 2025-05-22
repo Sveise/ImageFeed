5cebf22c837f3e1cd366ef28a5710cf5c93a3d28
Svetlana <sveise@mail.ru>
2025-05-22 16:44:29 +0200

    Добавлен файл Constants.swift с перечислениями

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Svetlana Varenova on 22.05.2025.
+//
+import Foundation
+
+enum Constants {
+    static let accessKey = "754364"
+    static let secretKey = "Xj0Q268920Nm15bC6uQNiDujq72FlJOptNbi6qAofgc"
+    static let redirectURI = "uz99zc3YcpGPAWG5w339v4F9j8ctIpUIq-HPyjH0j-s"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
