fc9927e1e8fceb044bfe44b876fea0c6ff632505
Svetlana <sveise@mail.ru>
2025-05-23 13:50:56 +0200

    Добавлен AuthViewController

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Svetlana Varenova on 23.05.2025.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+}
