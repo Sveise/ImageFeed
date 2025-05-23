cca0eed4cb572b7afcb6b984c8820c9409daea89
Svetlana <sveise@mail.ru>
2025-05-23 20:48:38 +0200

    WebView + AuthView

---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,80 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Svetlana Varenova on 23.05.2025.
+//
+
+import WebKit
+import UIKit
+
+protocol WebViewViewControllerDelegate: AnyObject {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String)
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController)
+}
+
+final class WebViewViewController: UIViewController {
+    
+    @IBOutlet private var webView: WKWebView!
+    
+    weak var delegate: WebViewViewControllerDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadAuthView()
+        webView.navigationDelegate = self
+    }
+    
+    private func loadAuthView() {
+        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else {
+            print("Ошибка")
+            return
+        }
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope)
+        ]
+        guard let url = urlComponents.url else {
+            print("Ошибка")
+            return
+        }
+        let request = URLRequest(url: url)
+        webView.load(request)
+        }
+    }
+    
+    extension WebViewViewController: WKNavigationDelegate {
+        func webView(
+            _ webView: WKWebView,
+            decidePolicyFor navigationAction: WKNavigationAction,
+            decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+        ) {
+            if let code = code(from: navigationAction) {
+                delegate?.webViewViewController(self, didAuthenticateWithCode: code)
+                decisionHandler(.cancel)
+            } else {
+                decisionHandler(.allow)
+            }
+        }
+        
+        private func code(from navigationAction: WKNavigationAction) -> String? {
+            if
+                let url = navigationAction.request.url,
+                let urlComponents = URLComponents(string: url.absoluteString),
+                urlComponents.path == "/oauth/authorize/native",
+                let items = urlComponents.queryItems,
+                let codeItem = items.first(where: { $0.name == "code" })
+            {
+                return codeItem.value
+            } else {
+                return nil
+            }
+        }
+    }
+
+
+enum WebViewConstants {
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+}
